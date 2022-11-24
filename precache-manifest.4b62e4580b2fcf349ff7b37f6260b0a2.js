c0c97a062d28170d93333aabe999723355cba49b
vientran199 <vientran1999@gmail.com>
2022-11-24 16:22:20 +0700

    Updates

---
+++ precache-manifest.4b62e4580b2fcf349ff7b37f6260b0a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "015ebb448bf137c845197db68c9a251f",
+    "url": "/Quan-ly-cong-viec/index.html"
+  },
+  {
+    "revision": "2d35741f9735e3d362d2",
+    "url": "/Quan-ly-cong-viec/static/css/main.2d506604.chunk.css"
+  },
+  {
+    "revision": "d6d95201bc6c5e2c31dc",
+    "url": "/Quan-ly-cong-viec/static/js/2.1229cfe6.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/Quan-ly-cong-viec/static/js/2.1229cfe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d35741f9735e3d362d2",
+    "url": "/Quan-ly-cong-viec/static/js/main.5863e00f.chunk.js"
+  },
+  {
+    "revision": "d7575ef245c63f5fb95e",
+    "url": "/Quan-ly-cong-viec/static/js/runtime-main.e46c2f10.js"
+  }
+]);
